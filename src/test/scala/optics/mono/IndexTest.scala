cf052db5713049d606debaf194afb9e065fdccbc
Julien Truffaut <truffaut.julien@gmail.com>
2020-02-27 16:37:58 +0000

    add test for both Index1 andd Index2

--- src/test/scala/optics/mono/IndexTest.scala
+++ src/test/scala/optics/mono/IndexTest.scala
@@ -9,11 +9,13 @@ class IndexTest extends munit.FunSuite {
   case class Foo(list: List[String])
 
   val list: Lens[Foo, List[String]] = Lens(_.list, xs => _.copy(list = xs))
+  val foo = Foo(List("a", "b", "c"))
 
-  test("indexMap") {
-    val foo = Foo(List("a", "b", "c"))
+  test("index1") {
+    assert((list >>> Index1.index(1)).getOrError(foo) == Right(1))
+  }
 
-//    assert((list >>> Index1.index(1)).getOrError(foo) == Right(1))
+  test("index2") {
     assert((list >>> Index2.index(1)).getOrError(foo) == Right(1))
   }
 
