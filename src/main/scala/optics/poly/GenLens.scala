e2c10ec4c1d0ddf9abebab398dc72b2b77330fef
Ken Scambler <ken.scambler@gmail.com>
2021-01-10 02:07:34 +1100

    Focus now works up to multiple nested fields

--- src/main/scala/optics/poly/GenLens.scala
+++ src/main/scala/optics/poly/GenLens.scala
@@ -14,8 +14,6 @@ object dsl {
     def idx(k: K)(using Index[A, K] { type To = B; type Error = Err }): B = ???
 }
 
-
-
 object GenLens {
 
   def impl[From: Type, To: Type](getterExpr: Expr[From => To])(using quotes: Quotes): Expr[Lens[From, To]] = {
@@ -85,10 +83,41 @@ object GenLens {
 
 object Focus {
 
-  def impl[S: Type, T: Type](getter: Expr[S => T])(using quotes: Quotes): Expr[Lens[S, T]] = {
+  def impl[From: Type, To: Type](getterExpr: Expr[From => To])(using quotes: Quotes): Expr[Lens[From, To]] = {
     import quotes.reflect._
   
-    /*
+
+    ///////////// DOMAIN //////////////////
+    case class ParseParams(argName: String, argType: TypeRepr, lambdaBody: Term)
+
+    enum FocusAction {
+      case Field(name: String, from: TypeRepr, to: TypeRepr)
+      //case Attempt(from: TypeRepr, to: TypeRepr)
+      //case Index(i: Term, indexType: TypeRepr, from: TypeRepr, to: TypeRepr)
+
+      def from: TypeRepr
+      def to: TypeRepr
+
+      override def toString(): String = this match {
+        case Field(name, from, to) => s"Field($name, ${from.show}, ${to.show})"
+      }
+    }
+
+    enum FocusError {
+      case NotACaseClass(className: String)
+      case NotAConcreteClass(className: String)
+      case DidNotDirectlyAccessArgument
+      case NotASimpleLambdaFunction
+      case UnexpectedCodeStructure(code: String)
+      case CouldntFindFieldType(fromType: String, fieldName: String)
+      case ComposeMismatch(type1: String, type2: String)
+
+      def asResult: FocusResult = Left(this)
+    }
+
+    type FocusResult = Either[FocusError, List[FocusAction]]
+
+
     ///////////// PARSING //////////////////
     def unwrap(term: Term): Term = {
       term match {
@@ -98,100 +127,137 @@ object Focus {
       }
     }
 
-    object ExpectedLambdaFunction {
-      def unapply(term: Term): Option[Term] = unwrap(term) match {
-        case Lambda(List(ValDef(_, _, _)), body) => Some(body)
-        case _ => None
-      }
+    object CaseClass {
+      def unapply(term: Term): Option[Term] =
+        term.tpe.classSymbol.flatMap { sym => 
+          Option.when(sym.flags.is(Flags.Case))(term)
+        }
     }
 
-    enum FocusAction {
-      case Field(name: String, from: TypeRepr, to: TypeRepr)
-      case Attempt
-      case Index(i: Term)
-    }
-
-    enum FocusError {
-      case NotACaseClass(className: String)
-      case NotAConcreteClass(className: String)
-      case NotASimpleLambdaFunction
-      case LambdaBodyIsWeird
-    }
-
-    type FocusResult = FocusError | List[FocusAction]
-
-    def parseLambdaBody(body: Term): FocusResult = {
-      def loop(remainingBody: Term, listSoFar: List[FocusAction]): FocusResult = {
-        remainingBody match {
-          case Select(prefix, "?") => FocusAction.Attempt :: listSoFar
-          case Select(ident @ Ident(_), fieldName) => 
-            ident.
-            FocusAction.Field(fieldName) :: listSoFar
-          case Select(prefix, fieldName) => loop(prefix, FocusAction.Field(fieldName) :: listSoFar)
-          case _ => FocusError.LambdaBodyIsWeird
+    //  val ValDef(field, typeTree, _) = t.classSymbol.get.memberField(x).tree
+    def getFieldType(fromType: TypeRepr, fieldName: String): Option[TypeRepr] =
+      fromType.classSymbol.flatMap { 
+        _.memberField(fieldName) match {
+          case sym if sym.isNoSymbol => None
+          case sym => sym.tree match {
+            case ValDef(_, typeTree, _) => Some(typeTree.tpe)
+            case _ => None
+          }
         }
       }
-      loop(body, Nil)
+
+    object ExpectedLambdaFunction {
+      def unapply(term: Term): Option[ParseParams] = 
+        unwrap(term) match {
+          case Lambda(List(ValDef(argName, typeTree, _)), body) => Some(ParseParams(argName, typeTree.tpe, body))
+          case _ => None
+        }
     }
 
-    ///////////// GENERATING //////////////////
-    def constructGetterBody(field: String, from: Term): Term = 
+    def parseLambdaBody(params: ParseParams): FocusResult = {
+      def loop(remainingBody: Term, listSoFar: List[FocusAction]): FocusResult = {
+
+        def addFieldAction(fromType: TypeRepr, fieldName: String): FocusResult = {
+          getFieldType(fromType, fieldName) match {
+            case Some(toType) => Right(FocusAction.Field(fieldName, fromType, toType) :: listSoFar)
+            case None => FocusError.CouldntFindFieldType(fromType.show, fieldName).asResult
+          }
+        }
+
+        remainingBody match {
+          // Leftmost select on the lambda argument
+          case Select(Ident(idName), fieldName) if idName == params.argName => addFieldAction(params.argType, fieldName)
+          case Select(Ident(idName), fieldName) => FocusError.DidNotDirectlyAccessArgument.asResult
+
+          // ? symbol to navigate the happy path of an Attempt
+          //case Select(prefix, "?") => loop(prefix, FocusAction.Attempt(prefix.tpe, ) :: listSoFar)
+
+          // Subsequent selects on field names
+          case Select(CaseClass(prefix), fieldName) => addFieldAction(prefix.tpe.widen, fieldName).flatMap(loop(prefix, _))
+          case Select(prefix, _) => FocusError.NotACaseClass(prefix.tpe.toString).asResult
+
+          case unexpected => FocusError.UnexpectedCodeStructure(unexpected.toString).asResult
+        }
+      }
+      loop(params.lambdaBody, Nil)
+    }
+
+    ///////////// CODE GENERATION //////////////////
+    def generateGetter(field: String, from: Term): Term = 
       Select.unique(from, field) // o.field
 
-    def constructSetterBody(field: String, from: Term, to: Term): Term = 
+    def generateSetter(field: String, from: Term, to: Term): Term = 
       Select.overloaded(from, "copy", Nil, NamedArg(field, to) :: Nil) // o.copy(field = value)
 
-    def constructLensExpr[From2: Type, To2: Type](field: String): Expr[Lens[From2, To2]] = 
-      '{
-        val setter = (to: To2) => (from: From2) => ${
-          constructSetterBody(field, Term.of('{from}), Term.of('{to})).asExprOf[From2]
-        }
-        
-        val getter = (from: From2) => ${ 
-          constructGetterBody(field, Term.of('{from})).asExprOf[To2] 
-        }
-        Lens.apply(getter, setter)
-      }
-
-    case class TypedLens(lens: Term, from: TypeRepr, to: TypeRepr)
-
-    def generateCode(actions: List[FocusAction]): Expr[Lens[From, To]] = {
-      def generateActionCode(action: FocusAction): Expr[Lens[From, To]] = 
-        action match {
-          case FocusAction.Field(name: String) => constructLensExpr(name)
-          case FocusAction.Attempt => '{ ??? }
-          case FocusAction.Index(idx) => '{ ??? }
-        }
-
-      actions.foldLeft('{Lens.id[To]}) { (lensExpr, action) => 
-        '{${lensExpr} >>> ${generateActionCode(action)}}
+    def generateLens(field: String, fromType: TypeRepr, toType: TypeRepr): Term = {
+      (fromType.asType, toType.asType) match {
+        case ('[f], '[t]) => 
+         '{
+            val setter = (to: t) => (from: f) => ${
+              generateSetter(field, '{from}.asTerm, '{to}.asTerm).asExprOf[f]
+            }
+            
+            val getter = (from: f) => ${ 
+              generateGetter(field, '{from}.asTerm).asExprOf[t] 
+            }
+            Lens.apply(getter, setter)
+          }.asTerm
       }
     }
-      
-    ///////////// EXECUTION //////////////////
-    val result: FocusResult = 
-      Term.of(getterExpr) match {
-        case ExpectedLambdaFunction(body) =>
-          TypeRepr.of[From].classSymbol match {
-            case Some(fromClass) if fromClass.flags.is(Flags.Case) => parseLambdaBody(body)
-            case Some(fromClass) => FocusError.NotACaseClass(fromClass.fullName)
-            case None => FocusError.NotAConcreteClass(Type.show[Type[From]])
+
+    def generateActionCode(action: FocusAction): Term = 
+      action match {
+        case FocusAction.Field(name, from, to) => generateLens(name, from, to)
+        //case FocusAction.Attempt => '{ ??? }.asTerm
+        //case FocusAction.Index(idx) => '{ ??? }.asTerm
+      }
+
+    def composeLensTerms(lens1: Term, lens2: Term): Term = {
+      (lens1.tpe.asType, lens2.tpe.asType) match {
+        case ('[EOptional[err1, from1, to1]], '[EOptional[err2, from2, to2]]) => 
+          '{ 
+            ${lens1.asExprOf[EOptional[err1, from1, to1]]} >>> ${lens2.asExprOf[EOptional[err2, to1, to2]]}
           }
-        case _ => FocusError.NotASimpleLambdaFunction
+
+        case ('[a], '[b]) =>
+          report.error(s"unable to compose ${Type.show[a]} >>> ${Type.show[b]}")
+          '{???}
+      }
+    }.asTerm
+
+    def generateCode(actions: List[FocusAction]): Term = {
+      actions.foldLeft('{Iso.id[From]}.asTerm) { (termSoFar, action) => 
+        composeLensTerms(termSoFar, generateActionCode(action))
+      }
+    }
+
+    ///////////// EXECUTION //////////////////
+    def errorMessage(error: FocusError): String = error match {
+      case FocusError.NotACaseClass(fromClass) => s"Expecting a case class in the 'From' position; found $fromClass"
+      case FocusError.NotAConcreteClass(fromClass) => s"Expecting a concrete case class in the 'From' position; cannot reify type $fromClass"
+      case FocusError.NotASimpleLambdaFunction => s"Expecting a lambda function that directly accesses a field. Example: `GenLens[Address](_.streetNumber)`"
+      case FocusError.DidNotDirectlyAccessArgument => s"Expecting a lambda function that directly accesses a field. Example: `GenLens[Address](_.streetNumber)`"
+      case FocusError.ComposeMismatch(type1, type2) => s"Could not compose $type1 >>> $type2"
+      case FocusError.UnexpectedCodeStructure(code) => s"Unexpected code structure: $code"
+      case FocusError.CouldntFindFieldType(fromType, fieldName) => s"Couldn't find type for $fromType.$fieldName"
+    }
+
+    val fromTypeIsConcrete = TypeRepr.of[From].classSymbol.isDefined
+    val result: FocusResult = 
+      getterExpr.asTerm match {
+        case ExpectedLambdaFunction(params) if fromTypeIsConcrete => parseLambdaBody(params)
+        case ExpectedLambdaFunction(_) => FocusError.NotASimpleLambdaFunction.asResult
+        case _ => FocusError.NotAConcreteClass(Type.show[Type[From]]).asResult
       }
 
-    ///////////// ERROR HANDLING //////////////////
     result match {
-      case List(actions: _*) => generateCode(action :: actions)
-      case FocusError.NotACaseClass(fromClass) => error(s"Expecting a case class in the 'From' position; found $fromClass"); '{???}
-      case FocusError.NotAConcreteClass(fromClass) => error(s"Expecting a concrete case class in the 'From' position; cannot reify type $fromClass"); '{???}
-      case FocusError.NotASimpleLambdaFunction => error(s"Expecting a lambda function that directly accesses a field. Example: `GenLens[Address](_.streetNumber)`"); '{???}
-      case FocusError.LambdaBodyIsWeird => error(s"Expecting a lambda function that directly accesses a field. Example: `GenLens[Address](_.streetNumber)`"); '{???}
+      case Right(list) => generateCode(list).asExprOf[Lens[From,To]]
+      case Left(error) => report.error(errorMessage(error)); '{???}
     }
-  }*/
+  }
 
 
-    
+    /*
     object Function {
       def unapply(t: Term): Option[(Term)] = t match {
         case Inlined(None, Nil, lambda @ Lambda(params, body)) => Some((lambda))
@@ -355,7 +421,7 @@ object Focus {
       List(TypeRepr.of[T]))
     )
     term.asExprOf[Lens[S,T]]
-  }
+  }*/
 
   def apply[S] = new MkFocus[S]
 
