57670c5a04a6c6bfe7037922e945de193c677fd7
Ken Scambler <ken.scambler@gmail.com>
2021-01-26 00:33:41 +1100

    Monomorphic support for polymorphic case classes

--- src/test/scala/optics/poly/FocusTest.scala
+++ src/test/scala/optics/poly/FocusTest.scala
@@ -12,9 +12,13 @@ final class FocusTest extends munit.FunSuite {
   case class Animal(name: String)
   case class Owner(pet: Animal)
   case class Shop(owner: Owner)
-
-  val fooLens: EOptional[NoSuchElementException, Shop, String] =
-    Focus[Shop](_.owner.pet.name)
+  case class Box[A](a: A) 
+  case class MultiBox[A,B](a: A, b: B)
+  case class HigherBox[F[_], A](fa: F[A])
+  case class RefinedBox[A](a: A) {type Banana}
+  case class UnionBox[A,B](aOrB: A | B)
+  case class ConstraintBox[A <: AnyVal](a: A)
+  case class Varargs[A](a: A*)
 
   test("Single field access") {
     assertEquals(
@@ -30,7 +34,51 @@ final class FocusTest extends munit.FunSuite {
     )
   }
 
+  test("Type parameter field access") {
+    assertEquals(
+      Focus[Box[String]](_.a).getOrError(Box("Hello")),
+      Right("Hello")
+    )
+  }
+
+  test("Type parameter set field") {
+    assertEquals(
+      Focus[Box[Int]](_.a).replace(111)(Box(222)),
+      Box(111)
+    )
+  }
+
+  test("Nested type parameter set field") {
+    assertEquals(
+      Focus[Box[Box[String]]](_.a.a).replace("hello")(Box(Box("ok"))),
+      Box(Box("hello"))
+    )
+  }
+
+  test("Multiple type parameters get field") {
+    assertEquals(
+      Focus[MultiBox[Int, Boolean]](_.b).get(MultiBox(222, true)),
+      true
+    )
+  }
+
+  test("Multiple type parameters set field") {
+    assertEquals(
+      Focus[MultiBox[String, Int]](_.a).replace("abc")(MultiBox("whatevs",222)),
+      MultiBox("abc", 222)
+    )
+  }
+
   /*
+  // We can't support this yet, because we don't know how to replace the type variables inside the `fa` value.
+  test("Higher kinded type parameter get field") {
+    assertEquals(
+      Focus[HigherBox[Option, Int]](_.fa).get(HigherBox(Some(23))),
+      Some(23)
+    )
+  }*/
+
+/*
   val fooLens: EOptional[NoSuchElementException, Foo, Int] =
     Focus[Foo](_.bar.?.fub.bab)
 
