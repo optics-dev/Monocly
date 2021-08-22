ba915b267642d5472207e10b2aaef6c1208383c2
Julien Truffaut <julien@fp-tower.com>
2021-08-22 21:15:41 +0200

    GetMany should not allow to call getOne

---
+++ src/test/scala/monocly/impl/GetterImplTest.scala
@@ -0,0 +1,23 @@
+package monocly.impl
+
+import munit.FunSuite
+
+class GetterImplTest extends munit.FunSuite {
+
+  test("GetMany getOne") {
+    val getter = GetManyImpl((x: List[Int]) => x)
+    val input = List(1,2,3)
+    assertEquals(
+      getter.get(input),
+      1
+    )
+  }
+
+  test("GetMany getOne doesn't compile") {
+    assertNoDiff(
+      compileErrors("GetManyImpl((x: List[Int]) => x).get(List(1,2,3))"),
+      "Error: ..."
+    )
+  }
+
+}
